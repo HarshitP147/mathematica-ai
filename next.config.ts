0f48fa2cd889691c863fb99d740cf04e8c315854
Harshit Pandit <harshit7757@gmail.com>
2025-10-06 22:04:33 +0100

    Adding supabase audio

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    // Exclude supabase functions folder from Next.js compilation
-    config.module = config.module || {};
-    config.module.rules = config.module.rules || [];
-    
-    config.module.rules.push({
-      test: /supabase[/\\]functions/,
-      loader: 'ignore-loader'
-    });
-
-    return config;
-  },
-  // Exclude supabase folder from transpilation
   transpilePackages: [],
 };
 
